5a47b107124916c64a0298d54c64e0716d790dcb
jombi <jombi@duck.com>
2024-12-24 11:47:29 +0900

    feat :: member - more information on dto

--- api/src/main/kotlin/com/dipvision/lora/api/member/dto/response/MemberInfoResponse.kt
+++ api/src/main/kotlin/com/dipvision/lora/api/member/dto/response/MemberInfoResponse.kt
@@ -1,5 +1,11 @@
 package com.dipvision.lora.api.member.dto.response
 
+import com.dipvision.lora.common.permission.Permissions
+
 data class MemberInfoResponse(
-    val name: String
+    val name: String,
+    val phone: String,
+    val groupName: String?,
+    val permissions: List<Permissions>,
+    val id: Long,
 )
