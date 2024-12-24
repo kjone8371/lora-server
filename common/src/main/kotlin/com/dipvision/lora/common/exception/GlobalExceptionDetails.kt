41fb0aff6482ab4aa46321a93a6513fc7fdb89de
jombi <jombi@duck.com>
2024-12-24 11:51:38 +0900

    fix :: GlobalExceptionDetails

--- common/src/main/kotlin/com/dipvision/lora/common/exception/GlobalExceptionDetails.kt
+++ common/src/main/kotlin/com/dipvision/lora/common/exception/GlobalExceptionDetails.kt
@@ -2,7 +2,7 @@ package com.dipvision.lora.common.exception
 
 import org.springframework.http.HttpStatus
 
-enum class GlobalExceptionDetail(override val message: String, override val status: HttpStatus) : ExceptionDetail {
+enum class GlobalExceptionDetails(override val message: String, override val status: HttpStatus) : ExceptionDetail {
     INTERNAL_SERVER_ERROR("내부 서버 에러", HttpStatus.INTERNAL_SERVER_ERROR),
 
     PARAMETER_NOT_MATCH("파라미터 불일치: %s (%s)", HttpStatus.BAD_REQUEST),
@@ -10,6 +10,9 @@ enum class GlobalExceptionDetail(override val message: String, override val stat
     UNPROCESSABLE_BODY("요청 바디가 없거나 읽을 수 없음", HttpStatus.BAD_REQUEST),
     UNSUPPORTED_MEDIA_TYPE("미디어 '%s'는 지원되지 않음. (지원하는 미디어: '%s')", HttpStatus.BAD_REQUEST),
     METHOD_NOT_SUPPORTED("메소드 '%s'(은)는 지원되지 않음. (지원하는 메소드: '%s')", HttpStatus.NOT_FOUND),
+    
+    INVALID_UUID("UUID 형식이 잘못 되었습니다.", HttpStatus.BAD_REQUEST),
+    NO_ENOUGH_PERMISSION("권한이 없습니다. '%s' 권한이 필요합니다.", HttpStatus.UNAUTHORIZED),
     ;
 
     override val code = name
