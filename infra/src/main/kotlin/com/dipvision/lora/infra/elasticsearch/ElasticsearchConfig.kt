06e1945b5e3af48a06862d9205fcaada7f467fbe
jombi <jombi@duck.com>
2024-12-27 15:48:28 +0900

    fix :: change pkcs12 to ca x.509 crt

--- infra/src/main/kotlin/com/dipvision/lora/infra/elasticsearch/ElasticsearchConfig.kt
+++ infra/src/main/kotlin/com/dipvision/lora/infra/elasticsearch/ElasticsearchConfig.kt
@@ -5,6 +5,7 @@ import org.springframework.core.io.DefaultResourceLoader
 import org.springframework.data.elasticsearch.client.ClientConfiguration
 import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration
 import java.security.KeyStore
+import java.security.cert.CertificateFactory
 import javax.net.ssl.SSLContext
 import javax.net.ssl.TrustManagerFactory
 import org.springframework.boot.autoconfigure.elasticsearch.ElasticsearchProperties as SpringElasticsearchProperties
@@ -18,23 +19,27 @@ class ElasticsearchConfig(
     override fun clientConfiguration(): ClientConfiguration {
         val builder = ClientConfiguration.builder()
             .connectedTo(*springElasticsearchProperties.uris.toTypedArray())
-        
+
         if (elasticsearchProperties.isSsl) {
-            val keyStore = KeyStore.getInstance("pkcs12")
-            DefaultResourceLoader().getResource(elasticsearchProperties.elasticCertPath).inputStream.use {
-                keyStore.load(it, elasticsearchProperties.elasticCertPassword.toCharArray())
-                keyStore
-            }
+            val certificate = DefaultResourceLoader()
+                .getResource(elasticsearchProperties.elasticCertPath)
+                .inputStream.use {
+                    CertificateFactory.getInstance("X.509").generateCertificate(it)
+                }
+            
+            val keyStore = KeyStore.getInstance(KeyStore.getDefaultType())
+            keyStore.load(null, null)
+            keyStore.setCertificateEntry("server", certificate)
 
             val trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm())
             trustManagerFactory.init(keyStore)
 
             val sslContext = SSLContext.getInstance("TLS")
             sslContext.init(null, trustManagerFactory.trustManagers, null)
-            
+
             builder.usingSsl(sslContext)
         }
-        
+
         return builder
             .withBasicAuth(springElasticsearchProperties.username, springElasticsearchProperties.password)
             .build()
