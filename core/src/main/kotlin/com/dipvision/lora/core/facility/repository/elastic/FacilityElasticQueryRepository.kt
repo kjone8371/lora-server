e325849c88e7ff495005d3cb8417f6803abd8a70
jeowon_1 <rlawjddnsj8371@gmail.com>
2025-01-10 11:55:42 +0900

    변경

--- core/src/main/kotlin/com/dipvision/lora/core/facility/repository/elastic/FacilityElasticQueryRepository.kt
+++ core/src/main/kotlin/com/dipvision/lora/core/facility/repository/elastic/FacilityElasticQueryRepository.kt
@@ -5,6 +5,8 @@ import com.dipvision.lora.core.common.pageable.ElasticPageRequest
 import com.dipvision.lora.core.facility.entity.FacilityDocument
 
 interface FacilityElasticQueryRepository {
+
+
     fun findByPoint(latitude: Double, longitude: Double, distance: Double): List<FacilityDocument>
     fun findByAddress(address: String, pageable: ElasticPageRequest): SlicedResponse<FacilityDocument>
 }
