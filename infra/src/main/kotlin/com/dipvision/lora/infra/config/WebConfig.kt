85686e3c6406e0beaeb0eee741443578b23fdac0
jeowon_1 <rlawjddnsj8371@gmail.com>
2025-01-11 20:01:53 +0900

    webconfig

--- infra/src/main/kotlin/com/dipvision/lora/infra/config/WebConfig.kt
+++ infra/src/main/kotlin/com/dipvision/lora/infra/config/WebConfig.kt
@@ -1,4 +1,16 @@
 package com.dipvision.lora.infra.config
 
-class WebConfig {
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfig : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins("http://218.233.244.111") // 허용할 출처 추가
+            .allowedMethods("GET", "POST", "PUT", "DELETE")
+            .allowedHeaders("*")
+            .allowCredentials(true)
+    }
 }
